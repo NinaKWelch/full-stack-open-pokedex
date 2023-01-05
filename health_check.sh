b56532ae0290da2270400f7ffe6a84b2b54a55e8
NinaWelch <nwelch1@live.co.uk>
2023-01-05 14:56:24 +0000

    Exercise 11.12.

---
+++ health_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# echo "Hello from shell script"
+
+# exit 0 # exit status 0 means that the script "succeeds" and 1 that the script "fails"
+
+response=$(curl --location --request GET "https://winter-flower-5452.fly.dev/health")
+
+if [[ $response == 'ok' ]]; then
+  exit 0
+fi
+
+if [[ $response != 'ok' ]]; then
+  exit 1
+fi
